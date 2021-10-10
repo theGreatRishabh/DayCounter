21bb1e264b33cf0d8a66b736eab9646fcc532a85
Rishabh Kumar <rk.rishabh.k@gmail.com>
2021-10-10 19:47:23 +0530

    Update script.js
    Updated date for 2022

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("October 10, 2021 09:29:59").getTime();
+var countDownDate = new Date("June 5, 2022 09:29:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
