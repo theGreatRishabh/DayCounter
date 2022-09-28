127c23f6a3e1f89e54e30419026ca0bed03de941
Rishabh Kumar <rk.rishabh.k@gmail.com>
2022-09-28 16:20:23 +0530

    Updated date for notice period

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("June 5, 2022 09:29:59").getTime();
+var countDownDate = new Date("November 15, 2022 09:29:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
